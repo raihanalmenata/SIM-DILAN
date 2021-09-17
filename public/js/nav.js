86e2b52eeb6cdfff5f8b249f1917858db1fc6542
Raihan Almenata <raihanalmenata@MacBook-Air-Raihan.local>
2021-09-17 07:49:05 +0700

    Navbar Added

---
+++ public/js/nav.js
@@ -0,0 +1,25 @@
+const toggle = document.querySelector('#toggle');
+const dropDown = document.querySelector('#floating-dropdown');
+
+const mobileToggle = document.querySelector('#mobile-toggle');
+const mobileDropdown = document.querySelector('#mobile-dropdown');
+
+toggle.addEventListener('click',()=>{
+    const dropped = dropDown.style.display;
+    
+    if(dropped == "" || dropped == "flex")
+        dropDown.style.display = "none";
+    else
+        dropDown.style.display = "flex";
+});
+
+
+mobileToggle.addEventListener('click',()=>{
+    const dropped = mobileDropdown.style.display;
+
+
+    if(dropped == "" || dropped == "flex")
+        mobileDropdown.style.display = "none";
+    else
+        mobileDropdown.style.display = "flex";
+});
